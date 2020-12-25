55292fb1b3f185ef34dbd9df8979cc7ead0f5610
unknown <rnjsguswo012@o365.hufs.ac.kr>
2020-12-25 21:47:35 +0900

    FEAT basic code

--- ArduinoWebServer/ArduinoWebServer.ino
+++ ArduinoWebServer/ArduinoWebServer.ino
@@ -1,1 +1,35 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
+const char* ssid = "";
+const char* password = "";
+
+ESP8266WebServer server(8080);
+
+void handleRoot() {
+  server.send(200, "text/html", "<h1>Hello ESP8266!!</h1>");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
